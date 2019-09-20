fb18b5bb98d8bbf448603f28e2b2d158bfc0d35c
419360214@qq.com <130681li>
2019-09-20 10:37:39 +0800

    优化代码

--- lib/src/main/java/com/mo/lib/GeTuiUtil.java
+++ lib/src/main/java/com/mo/lib/GeTuiUtil.java
@@ -1,6 +1,5 @@
 package com.mo.lib;
 
-import android.app.Service;
 import android.content.Context;
 
 import com.igexin.sdk.GTIntentService;
@@ -8,15 +7,20 @@ import com.igexin.sdk.GTIntentService;
 /**
  * @ author：mo
  * @ data：2019/9/19:16:11
- * @ 功能：
+ * @ 功能：个推工具类，在Application里调用
+ * @ 文档：http://docs.getui.com/getui/mobile/android/androidstudio_maven/
  */
 public class GeTuiUtil {
-
-public static <T2 extends GTIntentService> void initGeTui(Context var1, Class<T2> var3){
-    // DemoPushService 为【步骤2.6】自定义的推送服务
-    com.igexin.sdk.PushManager.getInstance().initialize(var1, KPushService.class);
-    // DemoIntentService 为第三方自定义的推送服务事件接收类
-    //        com.igexin.sdk.PushManager.getInstance().registerPushIntentService(getApplicationContext(), DemoIntentService.class);
-    com.igexin.sdk.PushManager.getInstance().registerPushIntentService(var1, var3==null?KIntentService.class:var3);
-}
+    /**
+     * 初始化个推SDK
+     * @param var1  上下文
+     * @param var3  消息处理Service (必须继承GTIntentService) 可null，
+     * @param <T2>  .
+     */
+    public static <T2 extends GTIntentService> void initGeTui(Context var1, Class<T2> var3) {
+        // DemoPushService 为【步骤2.6】自定义的推送服务
+        com.igexin.sdk.PushManager.getInstance().initialize(var1, KPushService.class);
+        // DemoIntentService 为第三方自定义的推送服务事件接收类
+        com.igexin.sdk.PushManager.getInstance().registerPushIntentService(var1, var3 == null ? KIntentService.class : var3);
+    }
 }
--- lib/src/main/java/com/mo/lib/KIntentService.java
+++ lib/src/main/java/com/mo/lib/KIntentService.java
@@ -13,52 +13,56 @@ import com.igexin.sdk.message.GTTransmitMessage;
  * @ author：mo
  * @ data：2019/9/18:10:44
  * @ 功能：接收推送
- *
+ * <p>
  * 继承 GTIntentService 接收来自个推的消息, 所有消息在线程中回调, 如果注册了该服务, 则务必要在 AndroidManifest中声明, 否则无法接受消息<br>
  */
 
 public class KIntentService extends GTIntentService {
-
     public KIntentService() {
     }
 
+    /** 程序启动时：1、接收pid */
     @Override
     public void onReceiveServicePid(Context context, int pid) {
     }
 
-    // 处理透传消息
+    /** 程序启动时：2、接收 clientId */
+    @Override
+    public void onReceiveClientId(Context context, String clientid) {
+        Log.i(GTIntentService.TAG, "onReceiveClientId -> " + "clientid = " + clientid);
+    }
+
+    /** 各种事件处理回执 */
+    @Override
+    public void onReceiveCommandResult(Context context, GTCmdMessage cmdMessage) {
+        Log.i(GTIntentService.TAG, "各种事件处理回执=" + "" + cmdMessage.toString());
+    }
+
+    /** 有消息时：0.离线上线通知 */
+    @Override
+    public void onReceiveOnlineState(Context context, boolean online) {
+    }
+
+    /** 有消息时：1.通知到达，只有个推通道下发的通知会回调此方法 */
+    @Override
+    public void onNotificationMessageArrived(Context context, GTNotificationMessage msg) {
+        Log.i(GTIntentService.TAG, "通知到达，只有个推通道下发的通知会回调此方法；" + "" + msg.toString());
+    }
+
+    /** 有消息时：2.通知点击，只有个推通道下发的通知会回调此方法 */
+    @Override
+    public void onNotificationMessageClicked(Context context, GTNotificationMessage msg) {
+        Log.i(GTIntentService.TAG, "通知点击，只有个推通道下发的通知会回调此方法；" + "" + msg.toString());
+    }
+
+    /**
+     * 有消息时：3.处理透传消息
+     * 透传消息获取方式：new String(msg.getPayload())，注意：1、msg.getPayload()有可能为null 2、这玩意是个键值对的json，还要转一下
+     * 第三方回执调用接口，actionid范围为90000-90999，可根据业务场景执行：PushManager.getInstance().sendFeedbackMessage(context, taskid, messageid, 90001)
+     */
     @Override
     public void onReceiveMessageData(Context context, GTTransmitMessage msg) {
 
     }
 
-    // 接收 cid
-    @Override
-    public void onReceiveClientId(Context context, String clientid) {
-        Log.i(GTIntentService.TAG,"onReceiveClientId -> " + "clientid = " + clientid);
-    }
-
-    // cid 离线上线通知
-    @Override
-    public void onReceiveOnlineState(Context context, boolean online) {
-    }
-
-    // 各种事件处理回执
-    @Override
-    public void onReceiveCommandResult(Context context, GTCmdMessage cmdMessage) {
-        Log.i(GTIntentService.TAG ,"各种事件处理回执="+""+cmdMessage.toString());
-
-    }
-
-    // 通知到达，只有个推通道下发的通知会回调此方法
-    @Override
-    public void onNotificationMessageArrived(Context context, GTNotificationMessage msg) {
-        Log.i(GTIntentService.TAG ,"通知到达，只有个推通道下发的通知会回调此方法；"+""+msg.toString());
-    }
-
-    // 通知点击，只有个推通道下发的通知会回调此方法
-    @Override
-    public void onNotificationMessageClicked(Context context, GTNotificationMessage msg) {
-        Log.i(GTIntentService.TAG ,"通知点击，只有个推通道下发的通知会回调此方法；"+""+msg.toString());
-    }
 }
